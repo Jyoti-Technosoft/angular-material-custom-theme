11b90e4a6d8f0f1c079202bd593fa0b3d3ba5935
dipesh gandhi <dipu961988@gmail.com>
2021-10-15 18:40:54 +0530

    Add like button and also customize it

--- src/app/my-modal/my-modal.component.ts
+++ src/app/my-modal/my-modal.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit,Input,Output, EventEmitter } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
 export interface DialogData {
   appversion: string;
   name: string;
@@ -10,9 +9,14 @@ export interface DialogData {
 @Component({
   selector: 'app-my-modal',
   templateUrl: './my-modal.component.html',
-  styleUrls: ['./my-modal.component.css']
+  styleUrls: ['./my-modal.component.scss']
 })
 export class MyModalComponent implements OnInit {
+  public favouriteApp:any =[{}];
+  
+  @Input()
+  selected: boolean = false;
+  @Output() selectedChange = new EventEmitter<boolean>();
 
   constructor(public dialogRef: MatDialogRef<MyModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
@@ -20,4 +24,14 @@ export class MyModalComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public toggleSelected(appname:string,appimage:string,version:string) {
+    this.selected = !this.selected;
+    this.selectedChange.emit(this.selected);
+    if(this.selected) {
+
+      // favouriteApp.push({favappname: appname, favappimage: appimage , favversion: version});
+      console.log(this.favouriteApp);
+    }    
+  }
+  
 }
